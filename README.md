42e4c09f865fb4a1f091c6283dd0964f032347fe
LeBuzzy <69692004+LeBuzzy@users.noreply.github.com>
2025-09-14 13:08:22 +0200

    Add rainbowsniffer SvelteKit film rating app

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pubcodex
+
+Ce dépôt contient l'application **rainbowsniffer**, un petit site de notation de films construit avec SvelteKit.
+
+Le code source de l'application se trouve dans le dossier [`rainbowsniffer`](rainbowsniffer).
---
+++ rainbowsniffer/src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  interface Film {
+    id: number;
+    title: string;
+    rating: number;
+  }
+
+  let films: Film[] = [
+    { id: 1, title: 'The Matrix', rating: 0 },
+    { id: 2, title: 'Inception', rating: 0 },
+    { id: 3, title: 'The Godfather', rating: 0 }
+  ];
+
+  function setRating(film: Film, rating: number) {
+    film.rating = rating;
+  }
+</script>
+
+<h1>rainbowsniffer - Film Ratings</h1>
+<ul>
+  {#each films as film}
+    <li>
+      <span>{film.title}</span>
+      {#each Array(5) as _, i}
+        <button on:click={() => setRating(film, i + 1)}>
+          {i < film.rating ? '★' : '☆'}
+        </button>
+      {/each}
+    </li>
+  {/each}
+</ul>
